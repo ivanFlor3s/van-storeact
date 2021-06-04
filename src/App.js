8a031135f49bb027015a6bb7b117e7b5cf228b8b
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-03 22:31:19 -0300

    Agrego import bootstrap.min.css y ya funciona Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {  Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 function App() {
