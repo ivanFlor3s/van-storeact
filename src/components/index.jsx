12d79e19f6d3e5fff8cdb82ebc6292b52317fa6a
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-07 22:59:37 -0300

    Creo card component

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+export const CardComponent= ()=>{
+    return (
+        <div>
+            <h1>
+                Titulo de Card
+            </h1>
+            <strong> $999,99</strong>
+        </div>
+    )
+}
