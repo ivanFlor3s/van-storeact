1f712bfcba3222e30d353d3639cd82cde00cdea3
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-10 00:44:05 -0300

    json con setting para crear components desde CLI + templates

---
+++ src/templates/default/TemplateName.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TemplateName.style.scss';
+
+const TemplateName = () => (
+  <>
+    <p>TemplateName component working :D </p>
+  </>
+);
+
+export default TemplateName;
