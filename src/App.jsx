d3d4b8aa9b48168bf4c35b9ca05b163dc670fef5
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-14 23:26:13 -0300

    Import de Componente en App para pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Container } from "react-bootstrap";
+import {Container, Col, Row } from "react-bootstrap";
 import { NavbarComponent } from "./components/NavbarComponent";
 import { ItemListContainer} from "./containers/ItemListContainer";
+import {ItemCount} from "./components/ItemCount"
+
 
 
 //TOSEE Api de los simpson - sandbox https://codesandbox.io/s/1zko6?file=/src/Character.js y api https://thesimpsonsquoteapi.glitch.me/
@@ -11,14 +13,21 @@ import { ItemListContainer} from "./containers/ItemListContainer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
     <NavbarComponent></NavbarComponent>
     
     <Container className="mt-3">
       <ItemListContainer greeting="Saludos desde App.jsx">
       </ItemListContainer>
     </Container>
-    
+    <Container>
+      <Row>
+        <Col xs= {5}>
+          <ItemCount></ItemCount>
+        
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -3,33 +3,13 @@ import "./ItemListContainer.style.scss";
 import { Card } from "../../components/Card";
 import { Row, Col } from "react-bootstrap";
 
-const CardList = [1,2,3,4,5,6,7];
-const link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 export const ItemListContainer = (props) => (
   <>
-  <div className = "ContainerList">
-    <h1> {props.greeting} </h1>
-    <hr />
-    <p>Map de homeros TEST</p>
-    <p>IF en MAP: href en <strong>Homero 6</strong>  </p>
+  <div className = "">
+
     <Row>
-      {CardList.map(( val, index) => {
-        if (index === 5 ) {
-          return (
-            <Col>
-              <Card   url={link}  indice={index + 1}></Card>
-            </Col>
-          );
-        }
-        else{
-          return (
-            <Col>
-              <Card  indice={index + 1}></Card>
-            </Col>
-          )
-        }
-      })}
+      <Card></Card>
     </Row>
 
   </div>
