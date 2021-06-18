4b1237fe8492af08ddb30bd28c0e717f0c6f3f20
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-17 22:58:47 -0300

    ItemListComponent

---
+++ src/containers/ItemList/ItemList.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import "./ItemList.style.scss";
+import { getDataMLA } from "../../utils/helpers";
+import { Container,Row, Col, Card, CardColumns} from "react-bootstrap";
+import { Item } from "../../components/Item/Item";
+
+export const ItemList = () => {
+  const [productos, setProds] = useState([]);
+
+  const setData = async () => {
+    const respuesta = await getDataMLA("Zapatillas");
+    //Agrego timeout para apreciar que hay una espera
+    setTimeout(() => {
+      let aux = respuesta.map((element) => {
+        return {
+          img: element.thumbnail,
+          title: element.title,
+          price: element.price,
+        };
+      });
+      setProds(aux);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    setData();
+    return () => {
+      console.log("Me muero");
+    };
+  }, []);
+
+  return (
+    <>
+      {productos.length == 0 ? "No tengo datos para mostrar" : "Tengo datos :)"}
+      <h1>Ahora vendemos zapatillas</h1>
+      <Container>
+
+        {/* <CardColumns>
+          {productos.map((element) => {
+            return <Item img = {element.img} title={element.title} price={element.price} ></Item>
+            })
+          }
+          
+        </CardColumns> */}
+        <Row>
+          {productos.map((element) => {
+            return (
+              <Col xs={4}>
+                <Item img = {element.img} title={element.title} price={element.price} ></Item>
+              </Col>
+              
+              )
+            })
+          }
+        </Row>
+      </Container>
+      
+        
+      
+    </>
+  );
+};
