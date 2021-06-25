e9c6d472f6dfd7256e0d49d5d270ea12ff8240f9
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-24 22:18:32 -0300

    format Codigo en Item component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import './Item.style.scss';
-import {Card, Button} from "react-bootstrap"
+import React from "react";
+import "./Item.style.scss";
+import { Card, Button } from "react-bootstrap";
 
-export const Item = ({title, img, price}) => {
-
-
+export const Item = ({ title, img, price }) => {
   return (
     <>
-      <Card className= "margin-card" style={{ width: '18rem' }}>
-          <Card.Img variant="top" src= {img} />
-          <Card.Body>
-            <Card.Title>{ title }</Card.Title>
-            <hr />
-            <h3>Precio: ${price}.00</h3>
-            <Card.Text>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque perspiciatis quas, corporis deleniti aperiam nesciunt exercitationem, repellendus nihil id assumenda aliquid consequatur voluptatem, harum quaerat dicta eveniet ex architecto sit.
-            </Card.Text>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" rel="noreferrer" target="_blank">
-              <Button variant="success" > Comprar</Button>
-
-            </a>
-          </Card.Body>
-        </Card>
+      <Card className="margin-card" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <hr />
+          <h3>Precio: ${price}.00</h3>
+          <Card.Text>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+            perspiciatis quas, corporis deleniti aperiam nesciunt
+            exercitationem, repellendus nihil id assumenda aliquid consequatur
+            voluptatem, harum quaerat dicta eveniet ex architecto sit.
+          </Card.Text>
+          <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Button variant="success"> Comprar</Button>
+          </a>
+        </Card.Body>
+      </Card>
     </>
-  )
+  );
 };
