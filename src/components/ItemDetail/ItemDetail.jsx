543e2aa2107fe294747d5ca18497ba1eadfe05fe
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-24 22:18:02 -0300

    Agrego estilo de Card al detail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./ItemDetail.style.scss";
-import { Col, Row, Image, Container,Badge } from "react-bootstrap";
+import { Col, Row, Image, Container, Badge, Card } from "react-bootstrap";
 import { ItemCount } from "../ItemCount";
+import StarRatings from "react-star-ratings";
+//https://codepen.io/ekeric13/project/editor/DkJYpA Sandbox de stars cmponents
 
 export const ItemDetail = ({ title, img, price }) => {
   const foo = (cantidad) => {
@@ -15,28 +17,49 @@ export const ItemDetail = ({ title, img, price }) => {
 
   return (
     <>
-      <Container>
-        <h1>{title}</h1>
-        <hr />
+      <Container className="mt-3">
         <Row>
           <Col xs={6}>
             <Image src={img} />
           </Col>
           <Col xs={4}>
-            <h2>
-              Precio <Badge className="badge-success" bg="success">${price}</Badge>
-            </h2>
-            
-            <h3>Descripcion</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              modi nihil dolor similique ipsa natus earum saepe, ullam facere
-              nisi animi quis ratione consequuntur, culpa, at ut itaque soluta
-              exercitationem
-            </p>
-            <ItemCount prod={productoMock} initial={1} onAdd={foo}>
-              
-            </ItemCount>
+            <Card>
+              <Card.Body>
+                <h2>{title}</h2>
+                <Row>
+                  <Col xs={6}>
+                    <StarRatings
+                      rating={4}
+                      numberOfStars={5}
+                      starRatedColor="rgb(13,110,253)"
+                      starDimension="15px"
+                      starSpacing="3px"
+                      starHoverColor="#ffffff"
+                    />
+                  </Col>
+                  <Col xs={6}>
+                    <h2>
+                      <Badge className="badge-success" bg="success">
+                        ${price}
+                      </Badge>
+                    </h2>
+                  </Col>
+                </Row>
+
+                <h3>Descripcion</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum modi nihil dolor similique ipsa natus earum saepe,
+                  ullam facere nisi animi quis ratione consequuntur, culpa, at
+                  ut itaque soluta exercitationem
+                </p>
+                <ItemCount
+                  prod={productoMock}
+                  initial={1}
+                  onAdd={foo}
+                ></ItemCount>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
