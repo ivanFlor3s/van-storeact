6f50761396c517c88e857f0b694989b369adb392
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-29 23:26:51 -0300

    Creo el componente Home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './Home.style.scss';
+import React from "react";
+import "./Home.style.scss";
+import {Jumbotron, Button} from "react-bootstrap"
 
 export const Home = () => {
-
-
   return (
     <>
-      <h1>Home component working :D </h1>
+      <div className="jumbotron animated fadeIn">
+        <h1>Hello, Home!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
     </>
-  )
+  );
 };
