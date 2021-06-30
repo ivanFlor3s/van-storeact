35872ecc2f0f109da0d7bacbe291ecd20cb6e1f4
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-29 23:28:51 -0300

    Modifico ItemListContainer  para recibir una cetogoria como parametro en url

--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -4,12 +4,16 @@ import { ItemCount } from "../../components/ItemCount";
 import { ItemList } from "../ItemList/ItemList";
 import { getDataMLA } from "../../utils/helpers";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { LoaderBurguer } from "../../shared/LoaderBurguer/LoaderBurguer";
 
 
 export const ItemListContainer = () => {
   const [productos, setProds] = useState([]);
+  const {categoryId}= useParams()
+
   const setData = async () => {
-    const respuesta = await getDataMLA("Zapatillas");
+    const respuesta = await getDataMLA(categoryId);
     //Agrego timeout para apreciar que hay una espera
     setTimeout(() => {
       let aux = respuesta.map((element) => {
@@ -21,19 +25,23 @@ export const ItemListContainer = () => {
         };
       });
       setProds(aux);
-    }, 1000);
+    }, 2000);
   };
 
   useEffect(() => {
+    setProds([])
     setData();
     return () => {
       console.log("Me muero");
     };
-  }, []);
+  }, [categoryId]);
 
   return (
   <>
-    {productos.length > 0 ?  <ItemList productosList={productos}></ItemList> : null} 
+     {productos.length > 0 ?  <ItemList productosList={productos} categoria={categoryId}></ItemList> : 
+      <LoaderBurguer></LoaderBurguer> }
+    
+    
   </>
   )
 };
