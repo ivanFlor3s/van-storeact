d3ba2c8c7f86a723a2103d210c9e33a7b07ca097
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-06-30 23:15:58 -0300

    Creo cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cart.style.scss';
+
+export const Cart = () => {
+
+
+  return (
+    <>
+    <h1 style={{textAlign:"center", backgroundColor: "yellow"}}>Cart Component</h1>
+      <img className="Cart" src="https://66.media.tumblr.com/22fea412b9c462934f38be15bad86989/35d1dfd7c22c8553-70/s500x750/20bab9dee14c7ed9eb3f6e026e6f969d0827083d.gif" alt="" />
+    </>
+  )
+};
