7e745566fa3a799d22f22a23b66d6ddec19bf406
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-08-01 00:42:24 -0300

    quito carousel de home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,22 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Home.style.scss";
 import { Image } from "react-bootstrap";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { ProductContext } from "../../context/ProductsContext";
-import { useContext, useState } from "react";
-import {Link} from "react-router-dom"
+
 
 export const Home = () => {
-  const productContext = useContext(ProductContext);
-
-  const [top, setTop] = useState([]);
-
-  useEffect(() => {
-    const TOP = productContext.productos.slice(0, 4);
-    setTop([...TOP]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  
   return (
     <>
       <div className="container-landing animated fadeIn ">
@@ -33,37 +21,7 @@ export const Home = () => {
           alt="landing-burguer"
           fluid
         />
-        <div className="carousel-home">
-          <div className="corusel-burguer">
-            <h1 className="text-nowrap">Algunos de nuestros productos</h1>
-            <Carousel width="100%" infiniteLoop={true} autoPlay={true}>
-              {top.length > 0 ? (
-                top.map((element, index) => {
-                  return (
-                    <div key={index}>
-                      
-                        <Image
-                          src={element.image}
-                          alt={element.title}
-                          fluid
-                          style={{height: '350px'}}
-                        />
-                      
-                      <Link to={`/item/${element.id}`}>
-
-                        <p className="legend">{element.title}</p>
-                      </Link>
-                    </div>
-                  );
-                })
-              ) : (
-                <div>
-                  <p className="legend">Sin nada que mostrar</p>
-                </div>
-              )}
-            </Carousel>
-          </div>
-        </div>
+       
       </div>
     </>
   );
